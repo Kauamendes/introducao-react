c8465c3d2b23855c8979d04e8ee1e1d227ae791f
Kauamendes <77807734+Kauamendes@users.noreply.github.com>
2021-07-05 08:06:10 -0300

    começando com react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+import Primeiro from './componentes/primeiro'
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render(
+    <Primeiro></Primeiro>,
+    document.getElementById('root')
+)
